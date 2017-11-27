2ba2f491a14220d0afd279cda7f22b4bcba9f804
kk126203 <kc3051@columbia.edu>
2017-11-27 03:20:56 -0500

    upload

---
+++ build_table.py
@@ -0,0 +1,146 @@
+import re, os, operator
+
+
+##Hhelper function for checking if an input string is Integer type
+def Irrelevant_Integer(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+
+## In this function, we process the words in the input file, remove some irrelavant words like 'and', 'of', 'in', and 'for', as well as the exclamation marks that is not needed. Then output the file as a list with remaining words splitted by space
+## The removed words are referenced in Top 50 English stop words
+def Clean_File(filename):
+    stopwords = open('./stop_words.txt').read()
+    p = re.compile('[\W_]+')
+    cur_file = open(filename, 'r').read().lower()
+    cur_file = p.sub(' ',cur_file)
+    re.sub(r'[\W_]+','', cur_file)
+    cur_file = cur_file.split()
+    cur_file = [w for w in cur_file if w not in stopwords]
+    irr_int = []
+    for w in cur_file :
+        if Irrelevant_Integer(w) and len(w)<4:              
+            irr_int.append(w)
+    cur_file = [w for w in cur_file if w not in irr_int]
+    return cur_file    
+
+
+
+## In this function, we caculate the index position of each word in the input list, so as to do the sentence matching for futher processing
+def Word_Position(input_list):
+    pos = {}
+    for i, word in enumerate(input_list):
+        if word in pos.keys():
+            pos[word].append(i)
+        else:
+            pos[word] = [i]
+            
+    return pos
+
+
+
+## In this function, we build a first table that contains a filename as a key, and a list of its words' position in the file as the table value. 
+def Build_Table():
+    table = {}
+    arr = os.listdir('./back-end')
+    for filename in arr:
+        dest = './back-end/'+filename
+        cur_file = Clean_File(dest)
+        cur_file = Word_Position(cur_file)
+        table[filename] = cur_file
+    return table
+
+
+
+## In this func, we transformed the table above to a final table that lets the words become the key of the final table, and the value is another inner table that contains a filename as its key, and a list that shows the position of the word in such file as the value of inner table.
+def Final_Table(input_table):
+    final_table = {}
+    for filename in input_table.keys():
+        for word in input_table[filename].keys():
+            if word in final_table.keys():
+                final_table[word][filename] = input_table[filename][word] 
+            else:
+                final_table[word] = {filename : input_table[filename][word]}
+    return final_table
+
+
+## This func implements the searching algorithm. First in each file, we extract the corresponding position list of each words of the input query, then we do the position match by subtracting its index position in the input query, then if the input query is completely in the file, there would be an integer shows up at every list, and this file will thus become the file we want.
+def Sentence_Match(input_table, query, table1):
+    stopwords = open('./stop_words.txt').read()
+    p = re.compile('[\W_]+')
+    query = p.sub(' ', query).lower()
+    l1, output, priority1, priority2 = [], [], [], []
+    arr = os.listdir('./back-end')
+
+    query_arr = [w for w in query.split() if w not in stopwords]
+    irr_int = []
+    for w in query_arr :
+        if Irrelevant_Integer(w) and len(w)<4:              
+            irr_int.append(w)
+    query_arr = [w for w in query_arr if w not in irr_int]
+
+    for word in query_arr:
+        for file in arr:
+            ff = str(file.lower())[:-4]
+            if word in ff and word not in output:
+                priority1.append(file)
+                
+    
+    for word in query_arr:
+        if word in input_table.keys():
+            l1.append(input_table[word].keys())
+    if len(l1)==0:
+        return []
+    
+    s1 = set(l1[0]).intersection(*l1)
+    u1 = set(l1[0]).union(*l1)
+    print(u1)
+    
+    for filename in s1:
+        tmp = []
+        for word in query_arr:
+            if word in input_table.keys():
+                tmp.append(input_table[word][filename])
+        for i in range(len(tmp)):
+            for j in range(len(tmp[i])):
+                tmp[i][j] -= i
+        if set(tmp[0]).intersection(*tmp):
+            priority2.append(filename)
+
+
+    for filename in priority1:
+        output.append(filename)
+
+    print output
+    
+    if len(query_arr)>1:
+        for filename in priority2:
+            if filename not in output:
+                output.append(filename)
+
+    print output
+    
+    u1 = [f for f in u1 if f not in output]
+    record = {}
+    for file in u1:
+        total = 0
+        for word in query_arr :
+            if word in table1[file].keys():
+                cur_size = len(table1[file][word])
+                total+=cur_size
+        record[file] = total
+        
+    record = sorted(record.items(), key=operator.itemgetter(1))
+    record = list(reversed(record))
+    for file in record:
+        if file not in output:
+            output.append(file[0])
+
+    print output
+    return output
+
+	
---
+++ search_engine.py
@@ -0,0 +1,100 @@
+import os, time
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+from build_table import *
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+final_table, table1 = [],[]
+ref = {'Avengers-AgeofUltron': ['Avengers-Age of Ultron (2015)', 'tt2395427'], 'GuardiansoftheGalaxyVol2': ['Guardians of the Galaxy Vol.2 (2017)', 'tt3896198'], 'TheAmazingSpiderMan2':['The Amazing Spider-Man 2 (2014)','tt1872181'],'ADogsPurpose':['A Dogs Purpose(2017)','tt1753383'], 'InsideOut':['Inside Out(2015)','tt2096673'],'TheFateoftheFurious':['The Fate of the Furious(2017)','tt4630562'], 'BeautyandtheBeast':['Beauty and the Beast(2017)','tt2771200'], 'Interstellar':['Interstellar (2014)','tt0816692'], 'TheHobbit-TheBattleoftheFiveArmies':['The Hobbit: The Battle of the Five Armies (2014)','tt2310332'], 'CaptainAmerica-CivilWar':['Captain America: Civil War (2016)','tt3498820'], 'It':['It (2017)','tt1396484'], 'TheHungerGames-Mockingjay-Part2':['The Hunger Games: Mockingjay - Part 2 (2015)','tt1951266'], 'TheHungerGames-Mockingjay-Part1':['The Hunger Games: Mockingjay - Part 1 (2014)','tt1951265'], 'CaptainAmerica-TheWinterSoldier':['Captain America: The Winter Soldier (2014)','tt1843866'], 'JurassicWorld':['Jurassic World','tt0369610'],'CatsandDogs':['Cats and Dogs (2001)','tt0239395'], 'JusticeLeague':['Justice League (2017)','tt0974015'], 'TheJungleBook':['The Jungle Book (2016)','tt3040964']}
+
+@app.before_request
+def before_request():
+  print "searh engine starts..."
+
+
+
+@app.teardown_request
+def teardown_request(exception):
+  print "search engine tears..."
+
+
+
+@app.route('/')
+def homepage():
+  return render_template("homepage.html")
+
+
+@app.route('/do_query_post', methods = ['POST'])
+def do_search():
+  start_time = time.time()
+  a, c, result = [],[],[]
+  query = str(request.form['query'])
+  tmp = Sentence_Match(final_table, query, table1)
+  for s in tmp:
+    result.append(str(s)[:-4])
+  for ll in result:
+    try:
+      b = []
+      b.append('http://www.imdb.com/title/'+ref[ll][1])
+      b.append(ref[ll][0])
+      a.append(b)
+    except:
+      print ref[ll]
+
+  num = len(result)
+  query = query.replace(" ","_")
+  c.append(query)
+  time_passed = time.time()-start_time
+  context = dict(data = a, data2 = c, data3 = num, data4 = time_passed)
+  return render_template("result.html", **context)
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8113, type=int)
+  def run(debug, threaded, host, port):
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
+  
+  table1 = Build_Table()
+  final_table = Final_Table(table1)
+  ref['DawnofthePlanetoftheApes'] = ['Dawn of the Planet of the Apes (2014)', 'tt2103281']
+  ref['Maleficent'] = ['Maleficent (2014)','tt1587310']
+  ref['TheMartian'] = ['The Martian (2015)','tt3659388']
+  ref['Deadpool'] = ['Deadpool (2016)','tt1431045']
+  ref['Minions'] = ['Minions (2015)', 'tt2293640']
+  ref['TheSecretLifeofPets'] = ['The Secret Life of Pets (2016)','tt2709768']
+  ref['DeathataFuneral'] = ['Death at a Funeral (2007)','tt0795368']
+  ref['Mission-Impossible-RogueNation'] = ['Mission: Impossible - Rogue Nation (2015)', 'tt2381249']
+  ref['Thor-Ragnarok'] = ['Thor: Ragnarok (2017)','tt3501632']
+  ref['DespicableMe3'] = ['Despicable Me 3 (2017)','tt3469046']
+  ref['PiratesoftheCaribbean-DeadMenTellNoTales'] = ['Pirates of the Caribbean: Dead Men Tell No Tales (2017)','tt1790809']
+  ref['Transformers-AgeofExtinction'] = ['Transformers: Age of Extinction (2014)','tt2109248']
+  ref['Dunkirk'] = ['Dunkirk (2017)', 'tt5013056']
+  ref['RogueOne'] = ['Rogue One (2016)','tt3748528']
+  ref['WolfWarrior2'] = ['Wolf Warrior II (2017)','tt7131870']
+  ref['FantasticBeastsandWheretoFindThem'] = ['Fantastic Beasts and Where to Find Them (2016)','tt3183660']
+  ref['Spectre'] = ['Spectre (2015)','tt2379713']
+  ref['WonderWoman'] = ['Wonder Woman (2017)', 'tt0451279']
+  ref['FindingDory'] = ['Finding Dory (2016)', 'tt2277860']
+  ref['SpiderMan-Homecoming'] = ['Spider-Man: Homecoming (2017)', 'tt2250912']
+  ref['X-Men-DaysofFuturePast'] = ['X-Men: Days of Future Past (2014)', 'tt1877832']
+  ref['Furious7'] = ['Furious 7 (2015)','Furious7']
+  ref['StarWars-TheForceAwakens'] = ['Star Wars: The Force Awakens (2015)', 'tt2488496']
+  ref['Zootopia'] = ['Zootopia (2016)','tt2948356']
+  ref['GuardiansoftheGalaxy'] = ['Guardians of the Galaxy (2014)', 'tt2015381']
+  ref['SuicideSquad'] = ['Suicide Squad (2016)', 'tt1386697']
+
+  
+  run()
+
+
